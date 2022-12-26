6ab27af436617b58eda4046a058b461bb2f1e508
PaulRichez <paul.richez59@gmail.com>
2022-12-26 07:37:47 +0100

    wip chart

--- client/src/app/dashboard/routes/etablishment/routes/stats/components/stats-card/stats-card.component.ts
+++ client/src/app/dashboard/routes/etablishment/routes/stats/components/stats-card/stats-card.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./stats-card.component.scss']
 })
 export class StatsCardComponent {
-
+  basicData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#42A5F5',
+            tension: .4
+        },
+        {
+            label: 'Second Dataset',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            fill: false,
+            borderColor: '#FFA726',
+            tension: .4
+        }
+    ]
+};
 }
